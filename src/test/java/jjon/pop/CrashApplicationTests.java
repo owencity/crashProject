0df7b2656a61e456682dc465fb585527abb07e26
owencity <skyko6530@gmail.com>
2024-08-07 00:41:11 +0900

    crash project

---
+++ src/test/java/jjon/pop/CrashApplicationTests.java
@@ -0,0 +1,13 @@
+package jjon.pop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
